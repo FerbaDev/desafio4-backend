53f465429cbbf53567ec01e116c43cc98b9644d3
FerbaDev <elferbarron1@gmail.com>
2024-03-01 01:24:08 -0300

    se pasan productos al home

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("home");
+const ProductManager = require("../controllers/productManager.js");
+const productManager = new ProductManager("./src/models/productos.json");
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    res.render("home", {products: products} );
+  } catch (error) {
+    console.log("Error al recuperar productos", error);
+    res.status(500).json({error: "Error interno del servidor"})
+  }
+  
 });
 
 router.get("/realTimeProducts", (req, res) => {
